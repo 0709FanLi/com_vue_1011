f2b2fce7b74e5dee8fe4ee05e86fbeb0f27cf1bd
liguangyuan <liguangyuan@mychery.com>
2021-12-10 00:22:21 +0800

    添加eslint

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router/index'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 createApp(App).use(ElementPlus).use(router).mount('#app')
--- src/views/axios.vue
+++ src/views/axios.vue
@@ -1,9 +1,12 @@
-<template></template>
+<template>
+  <div>1</div>
+</template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import axios from '../utils/axios'
+// import axios from '../utils/axios'
 
 export default defineComponent({
+  name: 'axiosSet',
   setup() {
     //   axios
     //     .get('/users/XPoet')
