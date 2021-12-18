11cdb60b33061881be4df1b0da1f0610d9174cdf
0709FanLi <385717045@qq.com>
2021-12-19 06:22:33 +0800

    实现选择

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/axios',
     name: 'Axios',
     component: () => import('@/views/axios.vue') // 懒加载组件
+  },
+  {
+    path: '/order',
+    name: 'Order',
+    component: () => import('@/views/order.vue')
   }
 ]
 
---
+++ src/views/order.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>欢迎关林红浪漫洗浴中心</h2>
+    <div>请选择一位美女为你服务</div>
+  </div>
+  <div style="margin-top: 15px">
+    <el-button
+      v-for="(item, key) in girls"
+      :key="key"
+      @click="selectGirlFunc(key)"
+      type="primary"
+      >{{ item }}</el-button
+    >
+  </div>
+  <h3>你选择了{{ selectGirl }}</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'orderCom',
+  setup() {
+    const girls = ref(['aaa', 'bbb', 'ccc'])
+    const selectGirl = ref('')
+    function selectGirlFunc(key: number): void {
+      selectGirl.value = girls.value[key]
+    }
+    return {
+      girls,
+      selectGirlFunc,
+      selectGirl
+    }
+  }
+})
+</script>
